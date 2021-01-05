e71fbef97d137e7a47f84b9ecae688f2113594a3
tcarnachan <63309989+tcarnachan@users.noreply.github.com>
2021-01-05 20:00:25 +0000

    Challenge A-1

--- r6th-Comp-Challenge/r6th-Comp-Challenge/Program.cs
+++ r6th-Comp-Challenge/r6th-Comp-Challenge/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
 namespace r6thCompChallenge
 {
@@ -6,7 +8,35 @@ namespace r6thCompChallenge
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool[] expectedResults = new bool[] { true, true, true, true, false, true, true, false, true, true };
+            string testFile = File.ReadAllText("Input Data/input_a1.txt").Replace("\r\n", "\n");
+            string[] testCases = testFile.Split(new string[] { "\n\n" }, StringSplitOptions.None);
+            Console.WriteLine(string.Join("\n", testCases.Zip(expectedResults, (test, result) =>
+            {
+                char[][] grid = test.Split('\n').Select(l => l.ToCharArray()).ToArray();
+                return $"Test data:\n{test}\nExpected Answer: {result}\nYour Answer: {Chain(grid)}\n";
+            })));
+        }
+
+        private static bool Chain(char[][] grid)
+        {
+            for(int i = 0; i < 3; i++)
+            {
+                // Check row
+                if (grid[i].Distinct().Count() == 1)
+                        return true;
+                // Check column
+                if (grid[0][i] == grid[1][i] && grid[1][i] == grid[2][i])
+                    return true;
+            }
+            // Check leading diagonal
+            if (grid[0][0] == grid[1][1] && grid[1][1] == grid[2][2])
+                return true;
+            // Check other diagonal
+            if (grid[0][2] == grid[1][1] && grid[1][1] == grid[2][0])
+                return true;
+            // No more possible 3-chains
+            return false;
         }
     }
 }

584934587da14d34448a1fe3aa5720db58cb37d3
tcarnachan <63309989+tcarnachan@users.noreply.github.com>
2021-01-12 20:47:45 +0000

    Challenge B1

--- r6th-Comp-Challenge/Program.cs
+++ r6th-Comp-Challenge/Program.cs
@@ -10,10 +10,11 @@ namespace r6thCompChallenge
         public static void Main(string[] args)
         {
             SectionA a = new SectionA();
-
             //TestA1(a);
+            //TestA2(a);
 
-            TestA2(a);
+            SectionB b = new SectionB();
+            TestB1(b);
         }
 
         private static void TestA1(SectionA a)
@@ -39,6 +40,32 @@ namespace r6thCompChallenge
                 char[][] grid = test.Split('\n').Select(l => l.ToCharArray()).ToArray();
                 bool result = possible.Contains(string.Concat(grid.Select(r => new string(r))));
                 Console.WriteLine($"Test data:\n{test}\nExpected Answer: {result}\nYour Answer: {a.TicTacToe(grid)}\n");
+            }
+        }
+
+        private static void TestB1(SectionB b)
+        {
+            string testFile = File.ReadAllText("Input Data/input_b1.txt").Replace("\r\n", "\n");
+            string[] testCases = testFile.Split(new string[] { "\n\n" }, StringSplitOptions.None);
+            foreach (string test in testCases)
+            {
+                char[][] grid = test.Split('\n').Select(l => l.ToCharArray()).ToArray();
+                Console.WriteLine("Test data:");
+                foreach(char[] row in grid)
+                {
+                    foreach(char c in row)
+                    {
+                        if (c == 'U')
+                            Console.ForegroundColor = ConsoleColor.Green;
+                        else
+                            Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write(c);
+                    }
+                    Console.WriteLine();
+                }
+                Console.ResetColor();
+                Console.WriteLine($"Your Answer: {b.MaxChain(grid)}\n");
+                //Console.WriteLine($"Test data:\n{test}\nYour Answer: {b.MaxChain(grid)}\n");
             }
         }
     }
---
+++ r6th-Comp-Challenge/SectionB.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace r6thCompChallenge
+{
+    public class SectionB
+    {
+        public int MaxChain(char[][] grid)
+        {
+            int maxChain = 0;
+
+            // Check rows
+            foreach(char[] row in grid)
+            {
+                int t = MaxChain(row);
+                if (t > maxChain)
+                    maxChain = t;
+            }
+
+            for(int i = 0; i < grid.Length; i++)
+            {
+                // Check column
+                var column = Enumerable.Range(0, grid.Length).Select(r => grid[r][i]);
+                int t = MaxChain(column.ToArray());
+                if (t > maxChain)
+                    maxChain = t;
+
+                // Check / diagonal
+                List<char> diagonal = new List<char>();
+                for (int offset = 0; i - offset >= 0; offset++)
+                    diagonal.Add(grid[offset][i - offset]);
+                t = MaxChain(diagonal.ToArray());
+                if (t > maxChain)
+                    maxChain = t;
+
+                // Check \ diagonal
+                diagonal.Clear();
+                for (int offset = 0; offset <= i; offset++)
+                    diagonal.Add(grid[i - offset][offset]);
+                t = MaxChain(diagonal.ToArray());
+                if (t > maxChain)
+                    maxChain = t;
+            }
+
+            return maxChain;
+        }
+
+        private int MaxChain(char[] row)
+        {
+            int start = -1, max = 0, end;
+            for (end = 0; end < row.Length; end++)
+            {
+                if (row[end] == 'U')
+                {
+                    if (start == -1)
+                        start = end;
+                }
+                else if (start != -1)
+                {
+                    int t = end - start;
+                    if (t > max)
+                        max = t;
+                    start = -1;
+                }
+            }
+            if (start != -1)
+                return Math.Max(end - start, max);
+            return max;
+        }
+    }
+}
